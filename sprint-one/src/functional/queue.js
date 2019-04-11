26fbce235548f6c75f8b9d396a3c759a71955cc9
Grahams Laptop <doughertyg37@gmail.com>
2019-04-10 23:36:42 -0700

    implemented the functional queue and stack methods as well as the functional-shared queue method

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
   var someInstance = {};
+  var size = 0;
+  var headIndex = 0;
+  var tailIndex = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,13 +10,29 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[tailIndex] = value;
+    tailIndex++;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if (size != 0) {
+      var removed = storage[headIndex];
+      delete storage[headIndex];
+      headIndex++;
+      size--;
+
+      return removed;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
+
+  someInstance.items = function() {
+    return storage;
+  }
 
   return someInstance;
 };
